fbf40eccf9d4d279d8fb8907b0c229501f450e47
Loliburta <mateusz.maranowski@protonmail.com>
2022-06-11 22:46:20 +0200

    add cast fetching and few more small changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { HomePage } from "./components/HomePage/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import MovieDetails from "./components/MovieDetails/MovieDetails";
+import { MovieDetails } from "./components/MovieDetails/MovieDetails";
 
 function App() {
   return (
